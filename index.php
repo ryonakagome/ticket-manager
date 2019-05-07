31a155f954d3bab414bbb9ea205ae3e26c1afcd6
Ryo Nakagome <nakagome@tento-net.com>
2019-05-07 14:30:56 +0900

    ログイン画面実装

--- index.php
+++ index.php
@@ -12,5 +12,20 @@
     <?php require_once('temp/head.php'); ?>
     <body>
         <?php require_once('temp/nav.php'); ?>
+        <div class="container">
+            <div class="row">
+                <div class="col s12">
+                    <h3>ログイン</h3>
+                    <p>N-Ticketを使用するには、ログインしてください...</p>
+                    <form action="d-login.php" method="POST">
+                        <h5>ユーザーID</h5>
+                        <input type="password" placeholder="ユーザーIDを入力..." name="ui" class="validate" required />
+                        <h5>パスワード</h5>
+                        <input type="password" placeholder="パスワードを入力..." name="pw" class="validate" required />
+                        <button type="submit" class="btn">送信</button>
+                    </form>
+                </div>
+            </div>
+        </div>
     </body>
 </html>
