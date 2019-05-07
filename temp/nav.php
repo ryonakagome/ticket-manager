65425e0224b17345a47bcc227192e4faf57fbb32
Ryo Nakagome <nakagome@tento-net.com>
2019-05-07 14:17:05 +0900

    ログインしていないときのナビゲーションを実装

--- temp/nav.php
+++ temp/nav.php
@@ -4,5 +4,15 @@
         print('<nav>');
         print('<div class="container">');
         print('<div class="nav-wrapper">');
-        print('<a href="#" class="brand-logo">N-Ticket</a>');
+        print('<a href="#!" class="brand-logo">N-Ticket</a>');
+        print('<a href="#" data-target="mobilemenu" class="sidenav-trigger"><i class="material-icons">menu</i></a>');
+        print('<ul class="right hide-on-med-and-down">');
+        print('<li><a href="http://n-point.pw">N-Point Webサイト</a></li>');
+        print('</ul>');
+        print('</div>');
+        print('</div>');
+        print('</nav>');
+    } else {
+
     }
+?>
