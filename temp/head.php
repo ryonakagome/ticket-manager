dc260faeb79d0f2d8537cb44cee9756ad6ad871d
Ryo Nakagome <nakagome@tento-net.com>
2019-05-07 14:13:14 +0900

    アイコンのWebフォントを追加

--- temp/head.php
+++ temp/head.php
@@ -9,5 +9,6 @@
 
         <!-- Materialize -->
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
         <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
     </head>
